0e88caf20b59e228dddba382530101d1e2f4145a
Umar Farouk Yusuf <umaryusuf11@gmail.com>
2022-03-14 19:30:08 +0000

    + added BinarySearchTree but extractMin not O(1)

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,13 +3,16 @@ package main;
 import main.Heap.MinHeap;
 import main.Queue.MinPriorityQueue;
 import main.Queue.MinPriorityQueueHeap;
+import main.Tree.BinarySearchTree;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = { 4, 15, 1, 4, 2, 5, 3, 1, 8, 6, 10 };
-
-        MinHeap minHeap = new MinHeap(arr);
-        System.out.println(minHeap);
+//        int[] arr = { 4, 15, 1, 4, 2, 5, 3, 1, 8, 6, 10 };
+//
+//        MinHeap minHeap = new MinHeap(arr);
+//        System.out.println(minHeap);
 //
 //        MinPriorityQueue minPriorityQueue = new MinPriorityQueue(8);
 //        minPriorityQueue.insert(5);
@@ -20,34 +23,57 @@ public class Main {
 //
 //        System.out.println(minPriorityQueue.extractMin());
 
-        MinPriorityQueueHeap minPriorityQueueHeap = new MinPriorityQueueHeap(8);
-        minPriorityQueueHeap.insert(5);
-        minPriorityQueueHeap.insert(4);
-        minPriorityQueueHeap.insert(6);
-        minPriorityQueueHeap.insert(3);
-        minPriorityQueueHeap.insert(1);
-        minPriorityQueueHeap.insert(3);
-        minPriorityQueueHeap.insert(2);
-        minPriorityQueueHeap.insert(1);
-        minPriorityQueueHeap.insert(-3);
+//        MinPriorityQueueHeap minPriorityQueueHeap = new MinPriorityQueueHeap(8);
+//        minPriorityQueueHeap.insert(5);
+//        minPriorityQueueHeap.insert(4);
+//        minPriorityQueueHeap.insert(6);
+//        minPriorityQueueHeap.insert(3);
+//        minPriorityQueueHeap.insert(1);
+//        minPriorityQueueHeap.insert(3);
+//        minPriorityQueueHeap.insert(2);
+//        minPriorityQueueHeap.insert(1);
+//        minPriorityQueueHeap.insert(-3);
+//
+//        System.out.println(minPriorityQueueHeap);
+//        System.out.println(minPriorityQueueHeap.extractMin());
+//
+//        System.out.println(minPriorityQueueHeap);
+//        System.out.println(minPriorityQueueHeap.extractMin());
+//
+//        System.out.println(minPriorityQueueHeap);
+//        System.out.println(minPriorityQueueHeap.extractMin());
+//
+//        System.out.println(minPriorityQueueHeap);
+//        System.out.println(minPriorityQueueHeap.extractMin());
+//
+//        System.out.println(minPriorityQueueHeap);
+//        System.out.println(minPriorityQueueHeap.extractMin());
+
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        binarySearchTree.insert(8);
+        binarySearchTree.insert(3);
+        binarySearchTree.insert(10);
+        binarySearchTree.insert(1);
+        binarySearchTree.insert(6);
+        binarySearchTree.insert(14);
+        binarySearchTree.insert(4);
+        binarySearchTree.insert(7);
+        binarySearchTree.insert(13);
+
+        System.out.println(Arrays.toString(binarySearchTree.toArray()));
+
+        System.out.println(binarySearchTree.extractMin());
+        System.out.println(binarySearchTree.extractMin());
+        System.out.println(binarySearchTree.extractMin());
+        System.out.println(binarySearchTree.extractMin());
+        System.out.println(binarySearchTree.extractMin());
+        System.out.println(binarySearchTree.extractMin());
+        System.out.println(binarySearchTree.extractMin());
+        System.out.println(binarySearchTree.extractMin());
+        System.out.println(binarySearchTree.extractMin());
 
 
+        System.out.println(Arrays.toString(binarySearchTree.toArray()));
 
-
-
-        System.out.println(minPriorityQueueHeap);
-        System.out.println(minPriorityQueueHeap.extractMin());
-
-        System.out.println(minPriorityQueueHeap);
-        System.out.println(minPriorityQueueHeap.extractMin());
-
-        System.out.println(minPriorityQueueHeap);
-        System.out.println(minPriorityQueueHeap.extractMin());
-
-        System.out.println(minPriorityQueueHeap);
-        System.out.println(minPriorityQueueHeap.extractMin());
-
-        System.out.println(minPriorityQueueHeap);
-        System.out.println(minPriorityQueueHeap.extractMin());
     }
 }
---
+++ src/main/Tree/BinarySearchTree.java
@@ -0,0 +1,132 @@
+package main.Tree;
+
+public class BinarySearchTree {
+// Implement the Binary Search Tree data structure including the following operations: search, min,
+// max, insert, and delete
+
+    private Node root;
+    private int size = 0;
+    private Node smallestNode;
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    public void insert(int value) {
+        insert(root, value);
+        size++;
+    }
+
+    private void insert(Node node, int value){
+        if (root == null){
+            root = new Node(value);
+            smallestNode = root;
+            return;
+        }
+        if(node.key > value){
+            if(node.left == null){
+                Node newNode = new Node(value, node);
+                node.left = newNode;
+
+                // check if new node is the smallest node, if so, update smallest node
+                if (newNode.key < smallestNode.key){
+                    smallestNode = newNode;
+                }
+                return;
+            }
+            insert(node.left, value);
+            return;
+        }
+        else {
+            if (node.right == null){
+                node.right = new Node(value, node);
+                return;
+            }
+            insert(node.right, value);
+            return;
+        }
+    }
+
+    public int min(){
+        return smallestNode.key;
+    }
+
+    public int extractMin(){
+        if (size == 0){
+            throw new IndexOutOfBoundsException("Tree is empty");
+        }
+        int min = smallestNode.key;
+        smallestNode = successor(smallestNode);
+        size--;
+        return min;
+    }
+
+    private Node minimum(Node node){
+        while (node.left != null){
+            node = node.left;
+        }
+        return node;
+    }
+
+    private Node successor(Node node){
+        if (node.right != null){
+            return minimum(node.right);
+        }
+        Node parent = node.parent;
+        while (parent != null && node == parent.right){
+            node = parent;
+            parent = parent.parent;
+        }
+        return parent;
+    }
+
+
+    private int left(int i){
+        return (i * 2) + 1;
+    }
+
+    private int right(int i){
+        return (i * 2) + 2;
+    }
+
+    public int[] toArray(){
+        // convert binary tree to array
+        int[] array = new int[100];
+        int index = 0;
+        Node node = root;
+        while (node != null){
+            array[index] = node.key;
+            index = left(index);
+            node = node.left;
+        }
+
+        index = 2;
+        node = root;
+        while (node != null){
+            array[index] = node.key;
+            index = right(index);
+            node = node.right;
+        }
+
+        return array;
+
+    }
+
+    private class Node {
+        int key;
+        Node left;
+        Node right;
+        Node parent;
+
+        private Node(int value) {
+            this.key = value;
+        }
+
+        private Node(int value, Node parent){
+            this.key = value;
+            this.parent = parent;
+        }
+    }
+}
+
+
